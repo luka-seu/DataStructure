5fe84411197eb1ac47e2c181bb2079c4fdfb03cc
luka_seu <dgm_wjy0515@seu.edu.cn>
2019-04-21 22:28:35 +0800

    旋转数组寻找目标数

--- src/main/java/com/plasticlove/array/ArrayContinuousSequence.java
+++ src/main/java/com/plasticlove/array/ArrayContinuousSequence.java
@@ -13,14 +13,18 @@ public class ArrayContinuousSequence {
         int high = 2;
         ArrayList<ArrayList<Integer>> result = new ArrayList();
         while(low<high){
+            //连续整数序列的求和公式
+            //(首项+末项)*项数/2
             int cur = (low+high)*(high-low+1)/2;
             if(cur==sum){
+                //获取当前序列的所有值
                 ArrayList<Integer> list = new ArrayList();
                 for(int i = low;i<=high;i++){
                     list.add(i);
                 }
                 result.add(list);
-                low++;//遍历其他序列
+                //遍历其他序列
+                low++;
             }else if(cur>sum){
                 low++;
             }else{
--- src/main/java/com/plasticlove/array/ArrayGreatestSumofSubArray.java
+++ src/main/java/com/plasticlove/array/ArrayGreatestSumofSubArray.java
@@ -1,15 +1,18 @@
 package com.plasticlove.array;
 
+import org.junit.Test;
+
 /**
  * @author luka-seu
  * @description 连续子数组的最大和
  * @create 2019/4/3-18:17
  */
 public class ArrayGreatestSumofSubArray {
-    public int FindGreatestSumOfSubArray(int[] array) {
+    public int findGreatestSumOfSubArray(int[] array) {
         int result = array[0];
         int total = array[0];
         for(int i =1;i<array.length;i++){
+            //如果当前的和小于零直接舍弃
             if(total<0){
                 total = array[i];
             }else{
@@ -20,5 +23,11 @@ public class ArrayGreatestSumofSubArray {
             }
         }
         return result;
+    }
+
+    @Test
+    public void test(){
+        int[] arr = {-4,-14,-3,12,-3,3,2,4,-5};
+        System.out.println(this.findGreatestSumOfSubArray(arr));
     }
 }
